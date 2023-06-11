6b34ff41293b86e02d7d0b7a966acfe454e89a94
Muhammad Nur Afif Aslami <nur.afif50@gmail.com>
2023-06-11 17:24:24 +0700

    Hover, Grab and Use Interactable

---
+++ AimAssault/Assets/ActivateTeleportationRay.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+
+public class ActivateTeleportationRay : MonoBehaviour
+{
+        public GameObject leftTeleportation;
+        public GameObject rightTeleportation;
+
+        public InputActionProperty leftActivate;
+        public InputActionProperty rigthActivate;
+
+        public InputActionProperty leftCancel;
+        public InputActionProperty rigthCancel;
+
+    // Start is called before the first frame update
+        
+    
+    // Update is called once per frame
+    void Update() 
+    {
+        leftTeleportation.SetActive(leftCancel.action.ReadValue<float>() == 0 &&  leftActivate.action.ReadValue<float>() > 0.1f);
+        rightTeleportation.SetActive(rightCancel.action.ReadValue<float>() == 0 &&  rigthActivate.action.ReadValue<float>() > 0.1f);
+    }
+}
