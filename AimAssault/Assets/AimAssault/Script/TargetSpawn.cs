0d20bcb4e853dfca4d59f4e89207c53b1cd5dd0e
Muhammad Nur Afif Aslami <nur.afif50@gmail.com>
2023-06-16 05:18:26 +0700

    TargetSpawn Update

---
+++ AimAssault/Assets/AimAssault/Script/TargetSpawn.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSpawn : MonoBehaviour
+{
+    public GameObject  theEnemy;
+    public int xPos;
+    public int zPos;
+    public int enemyCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       StartCoroutine (EnemyDrop());
+    }
+
+    IEnumerator EnemyDrop()
+    {
+        while(enemyCount <5)
+        {
+            xPos = Random.Range(34, 38);
+            zPos = Random.Range(39, 50);
+            Instantiate(theEnemy,new Vector3(xPos,4,zPos),Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            enemyCount += 1;
+        }
+    }
+}
