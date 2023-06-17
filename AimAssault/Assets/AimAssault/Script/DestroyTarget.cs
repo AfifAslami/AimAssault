9b18c04c9031929d2ce046d8683e2f689b2f9c47
Muhammad Nur Afif Aslami <nur.afif50@gmail.com>
2023-06-17 17:55:37 +0700

    Update targetspawn

--- AimAssault/Assets/AimAssault/Script/DestroyTarget.cs
+++ AimAssault/Assets/AimAssault/Script/DestroyTarget.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class DestroyTarget : MonoBehaviour
 {
+    private TargetSpawn spawn;
+
+    private void Start()
+    {
+        spawn = FindObjectOfType<TargetSpawn>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.CompareTag("Player"))
+        if (collision.collider.CompareTag("Enemy"))
         {
             Destroy(collision.gameObject);
+
+            // Kurangi jumlah musuh yang tersisa
+            spawn.targetCounter--;
+
+            // Jika jumlah musuh kurang dari 3, spawn musuh baru
+            if (spawn.targetCounter < 3)
+            {
+                spawn.SpawnRandomTarget();
+            }
         }
     }
 }
--- AimAssault/Assets/AimAssault/Script/TargetSpawn.cs
+++ AimAssault/Assets/AimAssault/Script/TargetSpawn.cs
@@ -4,26 +4,36 @@ using UnityEngine;
 
 public class TargetSpawn : MonoBehaviour
 {
-    public GameObject  theEnemy;
-    public int xPos;
-    public int zPos;
-    public int enemyCount;
+    public GameObject target;
+    float minSpawnPosZ = 37;
+    float maxSpawnPosZ = 47;
+    float spawnPosY = 4;
+    float minSpawnPosX = 35;
+    float maxSpawnPosX = 38;
+    public float startSpawning = 2.0f; 
+    public float minSpawningInterval = 1.0f;
+    public float maxSpawningInterval = 1.5f;
 
-    // Start is called before the first frame update
+    public int targetCounter = 0;
+
+    
     void Start()
     {
-       StartCoroutine (EnemyDrop());
+        InvokeRepeating("SpawnRandomTarget", startSpawning, Random.Range(minSpawningInterval, maxSpawningInterval));
     }
 
-    IEnumerator EnemyDrop()
-    {
-        while(enemyCount < 10)
-        {
-            xPos = Random.Range(35, 38);
-            zPos = Random.Range(39, 50);
-            Instantiate(theEnemy,new Vector3(xPos,4,zPos),Quaternion.identity);
-            yield return new WaitForSeconds(0.1f);
-            enemyCount += 2;
-        }
+    void Update()
+    {   
+    
     }
+
+    public void SpawnRandomTarget()
+    {
+        if (targetCounter < 3)
+        {
+            Vector3 spawnPos = new Vector3((Random.Range(minSpawnPosX,maxSpawnPosX)), spawnPosY, (Random.Range(minSpawnPosZ,maxSpawnPosZ)));
+            Instantiate(target, spawnPos, target.transform.rotation);
+            targetCounter++;
+        }
+    }
 }
